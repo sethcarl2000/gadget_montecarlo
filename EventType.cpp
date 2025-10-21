4bbbd04304dfee182b21ff992fe37802befc5365
sethcarl2000 <sethhall@jlab.org>
2025-10-21 13:28:06 -0400

    Add masses to event type definitions

--- EventType.cpp
+++ EventType.cpp
@@ -10,46 +10,54 @@ vector<EventType> EventType::Init(double enrichment_frac, double num_density)
 
     vector<EventType> types; 
 
+    const double dalt = GadgetUtils::one_dalton_in_MeV_c2; 
+
     // 235u elastic 
     types.push_back({
         kElastic, 
         [](double MeV){ return 8.4 * GadgetUtils::barns_to_cm; }, 
-        enrichment_frac*num_density
+        (1. - enrichment_frac)*num_density, 
+        235.*dalt
     }); 
 
-    // 238u inelastic
+    // 235u inelastic
     types.push_back({
         kInelastic, 
         [](double MeV){ return 1.6 * GadgetUtils::barns_to_cm; }, 
-        (1. - enrichment_frac)*num_density
+        (1. - enrichment_frac)*num_density, 
+        235.*dalt
     }); 
 
     // 235u fission 
     types.push_back({
         kFission, 
         [](double MeV){ return 1.6 * GadgetUtils::barns_to_cm; }, 
-        enrichment_frac*num_density
+        enrichment_frac*num_density, 
+        235.*dalt
     }); 
 
     // 238u elastic 
     types.push_back({
         kElastic, 
         [](double MeV){ return 9.4 * GadgetUtils::barns_to_cm; }, 
-        (1. - enrichment_frac)*num_density
+        (1. - enrichment_frac)*num_density, 
+        238.*dalt
     }); 
 
     // 238u inelastic
     types.push_back({
         kInelastic, 
         [](double MeV){ return 2.0 * GadgetUtils::barns_to_cm; }, 
-        (1. - enrichment_frac)*num_density
+        (1. - enrichment_frac)*num_density, 
+        238.*dalt
     }); 
 
     // 238u fission
     types.push_back({
         kFission, 
         [](double MeV){ return 0.06 * GadgetUtils::barns_to_cm; }, 
-        (1. - enrichment_frac)*num_density
+        (1. - enrichment_frac)*num_density, 
+        238.*dalt
     }); 
 
     return types; 
